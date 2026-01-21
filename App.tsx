fdb1548edcc7e2e44b897a1d3c073acbfcbedee8
khanhtaii19 <phamkhanhtaii@gmail.com>
2026-01-21 16:16:09 +0700

    Fix bug

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import BlogPost from './pages/BlogPost';
-import WelcomePopup from './components/WelcomePopup';
+import WelcomePopup from './components/WelcomePopup';   
 
 type Page = 'home' | 'blog';
 
